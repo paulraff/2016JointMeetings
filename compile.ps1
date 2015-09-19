8a5a364298dd7bfe67e80ce06a54e099686ce453
Paul Raff <paraff@microsoft.com>
2015-09-19 11:56:39 -0700

    Added final draft

---
+++ compile.ps1
@@ -0,0 +1,12 @@
+﻿Param
+(
+    [Parameter(Mandatory=$True)]
+    [string] $file
+)
+
+$dvi_file = $file.Replace(".tex", ".dvi")
+$ps_file = $file.Replace(".tex", ".ps")
+
+& latex $file
+& dvips -Ppdf $dvi_file
+& ps2pdf $ps_file
